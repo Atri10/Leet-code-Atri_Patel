10d8835de2c8b91470a26d5e5363b265bac51c89
Atri Patel <64589033+Atri10@users.noreply.github.com>
2021-09-22 22:15:40 +0530

    Time: 28 ms (99.60%), Space: 14.3 MB (18.21%) - LeetHub

--- count-square-sum-triples/count-square-sum-triples.py
+++ count-square-sum-triples/count-square-sum-triples.py
@@ -1,20 +1,5 @@
 class Solution:
-    def countTriples(self, n: int) -> int:
-        
-        ans = 0
-        sqrarr = []
-        carr = []
-        
-        for a in range(1,n+1):
-            for b in range(1,n+1):
-                sqrarr.append(a**2 + b**2)
-        
-        for c in range(1,n+1):carr.append(c**2)
-    
-        for i in sqrarr:
-            if i in carr:ans +=1
-        
-        return ans
+    def countTriples(self, n: int) -> int:return 2*sum(n//(x*x + y*y) for x in range(2, isqrt(n) + 1) for y in range(1, x) if (x&y&1) == 0 and gcd(x,y) == 1)
             
        
             
