8bd1a4a87ac8d726a7550ccd27a2660644751d45
Atri Patel <64589033+Atri10@users.noreply.github.com>
2021-09-23 23:46:18 +0530

    Time: 24 ms (98.25%), Space: 14 MB (90.39%) - LeetHub

--- calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.py
+++ calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.py
@@ -2,12 +2,11 @@ class Solution:
     def totalMoney(self, n: int) -> int:
         week  = n//7
         daysremain = n-week*7
-        ans = []
+        ans = 0
         for i in range(1,week+1):
-            t = [i for i in range(i,i+7)]
-            ans.append(sum(t))
-        t = [len(ans)+i+1 for i in range(daysremain)]
-        ans.append(sum(t))
-        return sum(ans)
+            ans+=sum(range(i,i+7))
+        t = [week+i+1 for i in range(daysremain)]
+        ans +=sum(t)
+        return ans
        
             
