7123d5d93b3737d7e02cf845c23ef64fb72e4dcc
Atri Patel <64589033+Atri10@users.noreply.github.com>
2021-09-27 12:20:55 +0530

    Time: 640 ms (97.06%), Space: 18.1 MB (24.28%) - LeetHub

--- 3sum/3sum.py
+++ 3sum/3sum.py
@@ -1,27 +1,25 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        
-        nums.sort()
-        triplets = []
-       
-       
-        for i in range(len(nums)-2):
+        n = []
+        p = []
+        z = []
+        res = set()  
+        for i in nums:
+            if i>0:p.append(i)
+            elif i==0:z.append(i)
+            else:n.append(i)
+        N,P = set(n),set(p)
+        if z:
+            for i in P:
+                if -1*i in N:res.add((-1*i,0,i))
+        if len(z)>=3:res.add((0,0,0))
+        for i in range(len(n)):
+            for j in range(i+1,len(n)):
+                T = -1*(n[i]+n[j])
+                if T in P : res.add(tuple(sorted([n[i],n[j],T])))
+        for i in range(len(p)):
+            for j in range(i+1,len(p)):
+                T = -1 * (p[i]+p[j])
+                if T in N: res.add(tuple(sorted([p[i],p[j],T]))) 
+        return res
             
-            left = i+1
-            right = len(nums)-1
-            print(i,left,right)
-            while left < right:
-                T = nums[i] + nums[left] + nums[right]
-                if T ==0:
-                    ans = [nums[i],nums[left],nums[right]]
-                    if ans not in triplets :triplets.append(ans)
-                    left+=1
-                    right -=1
-                
-                elif T>0:right -=1
-                else: left+=1
-        
-        
-        return triplets
-                    
-                
