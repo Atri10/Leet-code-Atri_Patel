2b3bf2c20df70cb6ddb8bb556909c83cd0665ce7
Atri Patel <64589033+Atri10@users.noreply.github.com>
2021-10-10 07:55:23 +0530

    Time: 35 ms (34.19%), Space: 14.4 MB (16.92%) - LeetHub

--- powx-n/powx-n.py
+++ powx-n/powx-n.py
@@ -1,12 +1,21 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        from functools import cache
-        @cache
+        
+        memo = {}
+        
         def power(x,n):
+            if n in memo:return memo[n]
+
             if n==0: return 1
             elif n==1:return x
-            elif n < 0: return power(1/x,-n)
-            elif n%2==0:return power(x*x,n//2)
-            else: return x * power(x*x,(n-1)//2)
+            elif n < 0:
+                memo[n] = power(1/x,-n)
+                return memo[n]
+            elif n%2==0:
+                memo[n] = power(x*x,n//2)
+                return memo[n]
+            else:
+                memo[n] = x * power(x*x,(n-1)//2)
+                return memo[n]
             
         return power(x,n)
