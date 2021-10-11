c39bc497571f04297ae02221572cbaf67b96d001
Atri Patel <64589033+Atri10@users.noreply.github.com>
2021-10-11 12:46:36 +0530

    Time: 592 ms (87.14%), Space: 27.7 MB (74.00%) - LeetHub

--- contains-duplicate-ii/contains-duplicate-ii.py
+++ contains-duplicate-ii/contains-duplicate-ii.py
@@ -1,14 +1,7 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
         memo = {}
-        minn = 999999
-        maxx = 0
-        arr = []
-        for i in range(len(nums)):
-            if nums[i] in memo:
-                minn = memo[nums[i]]
-                maxx = max(maxx,i)
-                memo[nums[i]] = i
-                arr.append(abs(minn-maxx) <= k)
-            else:memo[nums[i]] = i 
-        return (True in arr)
+        for i in range(len(nums)):    
+            if nums[i] in memo and abs(memo[nums[i]] - i) <=k : return True          
+            else:memo[nums[i]]=i
+        return False
