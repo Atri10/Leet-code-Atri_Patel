182a4776c0cb1baf5a6019f52bf4223187fc6dc8
Atri Patel <64589033+Atri10@users.noreply.github.com>
2021-10-25 22:31:17 +0530

    Time: 69 ms (60.42%), Space: 14.5 MB (29.00%) - LeetHub

--- queries-on-a-permutation-with-key/queries-on-a-permutation-with-key.py
+++ queries-on-a-permutation-with-key/queries-on-a-permutation-with-key.py
@@ -1,13 +1,11 @@
 class Solution:
     def processQueries(self, queries: List[int], m: int) -> List[int]:
-        P = []
+        P = [i+1 for i in range(m)]
         ans = []
-        for i in range(m):P.append(i+1)
         for i in range(len(queries)):
-            el = queries[i]
-            ans.append(P.index(el))
-            P.remove(el)
-            P.insert(0,el)
+            ans.append(P.index(queries[i]))
+            P.remove(queries[i])
+            P.insert(0,queries[i])
         return ans
 
         
