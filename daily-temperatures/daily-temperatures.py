79b80e0eb30f6872dbbd775ec259a894f5371723
Atri Patel <64589033+Atri10@users.noreply.github.com>
2021-11-14 00:40:49 +0530

    Time: 1192 ms (93.26%), Space: 25.6 MB (23.68%) - LeetHub

---
+++ daily-temperatures/daily-temperatures.py
@@ -0,0 +1,14 @@
+class Solution:
+    def dailyTemperatures(self, T: List[int]) -> List[int]:
+        n, right_max = len(T), float('-inf')
+        res = [0] * n
+        for i in range(n-1, -1, -1):
+            t = T[i]
+            if right_max <= t:
+                right_max = t
+            else:
+                days = 1
+                while T[i+days] <= t:
+                    days += res[i+days]
+                res[i] = days
+        return res
