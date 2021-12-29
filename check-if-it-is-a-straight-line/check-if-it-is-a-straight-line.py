419d19ae44f6beaa0c2bf2ada2f0ac66db10e87e
Atri Patel <64589033+Atri10@users.noreply.github.com>
2021-12-29 13:31:08 +0530

    Time: 87 ms (10.04%), Space: 14.8 MB (46.03%) - LeetHub

--- check-if-it-is-a-straight-line/check-if-it-is-a-straight-line.py
+++ check-if-it-is-a-straight-line/check-if-it-is-a-straight-line.py
@@ -1,18 +1,6 @@
 class Solution:
     def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
-        a,b = coordinates[0]
-        c,d = coordinates[1]
-        
-        if c-a  == 0 : ogslop = float('inf')
-        else:ogslop = int((d-b) // (c-a))
-        
-        for i in range(1,len(coordinates)-1):
-            a,b = coordinates[i]
-            c,d = coordinates[i+1]
-            
-            if c-a  == 0 : slop = float('inf')
-            else:slop = int((d-b) // (c-a))
-            
-            if slop != ogslop : return False
-            
+        (a,b) , (c,d) = coordinates[:2]
+        for x,y in coordinates:
+            if (y-d)*(c-a) != (x-c)*(d-b) : return False
         return True
