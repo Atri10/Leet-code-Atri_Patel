8f8aa6569be9e98318394cc8afb2380d483c1af3
Atri Patel <64589033+Atri10@users.noreply.github.com>
2022-01-09 22:20:31 +0530

    Time: 88 ms (86.45%), Space: 23.4 MB (48.15%) - LeetHub

--- 143-reorder-list/143-reorder-list.py
+++ 143-reorder-list/143-reorder-list.py
@@ -1,8 +1,5 @@
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
-        """
-        Do not return anything, modify head in-place instead.
-        """
         arr = []
         temp =  head
         while temp:
@@ -19,7 +16,5 @@ class Solution:
             else :
                 head.next = arr[r]
                 r -= 1
-                
             head = head.next
-            
         head.next = None
