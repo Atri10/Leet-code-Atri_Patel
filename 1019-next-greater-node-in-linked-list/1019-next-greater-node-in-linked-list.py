162a94d1cf99f1a2aecb5f800c5ea3bd62d2819d
Atri Patel <64589033+Atri10@users.noreply.github.com>
2022-01-09 21:32:57 +0530

    Time: 432 ms (19.74%), Space: 18.5 MB (56.39%) - LeetHub

--- 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.py
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.py
@@ -3,9 +3,11 @@ class Solution:
         stack = []
         ans = []
         idx = 0
+        
         while head:
             ans.append(0)
             cur_val = head.val
+            
             while stack and stack[-1][0] < cur_val:
                 last_val = stack.pop()
                 ans[last_val[1]] = cur_val
