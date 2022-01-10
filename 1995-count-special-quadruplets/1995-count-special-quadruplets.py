66017e13901cad96245af0a00d3ff1c05f6ea9ee
Atri Patel <64589033+Atri10@users.noreply.github.com>
2022-01-10 21:57:48 +0530

    Time: 2233 ms (12.38%), Space: 14.2 MB (80.29%) - LeetHub

---
+++ 1995-count-special-quadruplets/1995-count-special-quadruplets.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countQuadruplets(self, nums: List[int]) -> int:
+        
+        ans = 0
+        
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                for k in range(j+1,len(nums)):
+                    for l in range(k+1,len(nums)):
+                        
+                        if nums[i] + nums[j] + nums[k] == nums[l] : ans += 1
+        return ans
