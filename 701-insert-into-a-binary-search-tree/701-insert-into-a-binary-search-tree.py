1c8977b5913af46fe860b1c84b2375b63fa1b8c5
Atri Patel <64589033+Atri10@users.noreply.github.com>
2022-01-12 18:58:44 +0530

    Time: 237 ms (7.47%), Space: 24.4 MB (26.36%) - LeetHub

--- 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.py
+++ 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.py
@@ -1,4 +1,7 @@
+from functools import cache
+
 class Solution:
+    @cache
     def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
         if not root : return TreeNode(val)
         if root.val < val : root.right = self.insertIntoBST(root.right, val)
