168c0ede6d685f9a135f6f62517b163b8467ac9a
Atri Patel <64589033+Atri10@users.noreply.github.com>
2022-01-14 23:44:57 +0530

    Time: 77 ms (27.92%), Space: 18.5 MB (32.49%) - LeetHub

--- 501-find-mode-in-binary-search-tree/501-find-mode-in-binary-search-tree.py
+++ 501-find-mode-in-binary-search-tree/501-find-mode-in-binary-search-tree.py
@@ -1,16 +1,11 @@
 class Solution:
     def findMode(self, root: Optional[TreeNode]) -> List[int]:
-        
         dic = {}
-        
         def pre(root):
-            
             if root:
                 pre(root.left)
-                
                 if root.val in dic : dic[root.val] =  dic[root.val] + 1
                 else : dic[root.val] = 1
-                
                 pre(root.right)
                 
         pre(root)
