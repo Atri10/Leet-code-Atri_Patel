66778e4a375767c6ed7bdeaa5edfa059a6f03268
Atri Patel <64589033+Atri10@users.noreply.github.com>
2022-01-15 20:19:28 +0530

    Time: 128 ms (25.36%), Space: 20.5 MB (13.03%) - LeetHub

---
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.py
@@ -0,0 +1,27 @@
+class Solution:
+    def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
+        
+        arr = []
+        
+        def towsum(arr):
+            
+            dic = {}
+            
+            for i in arr:
+                x = k - i
+                if x in dic : return True
+                else : dic[i] = True
+            
+            print(dic)
+            
+            return False
+                    
+        def rec(root):
+            if root:
+                rec(root.left)
+                arr.append(root.val)
+                rec(root.right)
+                
+        rec(root)
+        
+        return towsum(arr)
