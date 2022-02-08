434c54823882ac4db9cd7874806128246deffb44
Atri Patel <64589033+Atri10@users.noreply.github.com>
2022-02-08 09:28:11 +0530

    Time: 75 ms (5.68%), Space: 14.1 MB (75.03%) - LeetHub

--- 258-add-digits/258-add-digits.py
+++ 258-add-digits/258-add-digits.py
@@ -1,16 +1,8 @@
 class Solution:
     def addDigits(self, num: int) -> int:
-        
-        
         def rec(n):
-            
             x = str(n)
-            
             s = sum([int(i) for i in x])
-            
             if s < 10 : return s
-            
-            else:return rec(s)
-            
-            
+            return rec(s)
         return rec(num)
