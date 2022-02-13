56ba1b49dc22fcad31d63e63c7b13bf376b7aee9
Atri Patel <64589033+Atri10@users.noreply.github.com>
2022-02-13 20:28:20 +0530

    Time: 55 ms (31.85%), Space: 13.9 MB (99.94%) - LeetHub

---
+++ 78-subsets/78-subsets.py
@@ -0,0 +1,7 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        result = [[]]
+        for num in nums:
+            result += [i + [num] for i in result]
+        return result
