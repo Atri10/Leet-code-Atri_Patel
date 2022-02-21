15d749e94d935bc0b2c3c702ca462a9a3e25f11b
Atri Patel <64589033+Atri10@users.noreply.github.com>
2022-02-21 15:32:46 +0530

    Time: 323 ms (11.59%), Space: 15.6 MB (8.22%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,10 +1,5 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        
-        dic = {}
-        
-        for i in nums:
-            if i in dic : dic[i] = dic[i] + 1
-            else: dic[i] = 1
-        
-        return max(zip(dic.keys(),dic.values()),key = lambda i : i[1])[0]
+        nums.sort()
+        ans = nums[len(nums)//2]
+        return ans
