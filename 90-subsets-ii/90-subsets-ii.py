77c00bd27eb8de6a523d077015463e72b926c49d
Atri Patel <64589033+Atri10@users.noreply.github.com>
2022-03-05 18:37:55 +0530

    Time: 72 ms (18.69%), Space: 14.2 MB (71.45%) - LeetHub

---
+++ 90-subsets-ii/90-subsets-ii.py
@@ -0,0 +1,22 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        
+        res = []
+        subsets = []
+        
+        def sub(i):
+            
+            if i >= len(nums):
+                temp = sorted(res.copy())
+                if temp not in subsets:
+                    subsets.append(temp)
+                return 
+            
+            res.append(nums[i])
+            sub(i+1)
+            res.pop()
+            sub(i+1)
+            
+        
+        sub(0)
+        return sorted(subsets)
