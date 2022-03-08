ea1ebd27feb066328ffb8133c9e94a88680d7fe4
Atri Patel <64589033+Atri10@users.noreply.github.com>
2022-03-08 13:44:50 +0530

    Time: 56 ms (40.97%), Space: 16.3 MB (22.50%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.py
+++ 206-reverse-linked-list/206-reverse-linked-list.py
@@ -8,22 +8,10 @@ class Solution:
             head = head.next
         
         dummy = n = ListNode(-1)
-        
-        
-        
+         
         for i in arr[::-1]:
             node = ListNode(i)
             n.next = node
             n = n.next
         
         return dummy.next
-        
-        
-        prev = None
-        nextt = None
-        while head:
-            nextt = head.next
-            head.next = prev
-            prev = head
-            head = nextt
-        return prev
