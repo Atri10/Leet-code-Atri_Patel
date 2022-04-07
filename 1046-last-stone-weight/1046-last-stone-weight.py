aabae1cc9add47c69e0e160a360727ec22ea745e
Atri Patel <64589033+Atri10@users.noreply.github.com>
2022-04-07 15:32:35 +0530

    Time: 48 ms (42.93%), Space: 14 MB (17.07%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -0,0 +1,46 @@
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        
+        if len(stones) == 1 :
+            return sum(stones)
+        
+        stones.sort()
+        
+        x = stones[-1]
+        y = stones[-2]
+        
+        
+        while len(stones) > 2:
+            
+            if x > y:
+                stones[-2] = x - y
+                stones[-1] = 0
+            
+            elif x == y:
+                stones[-1] = 0
+                stones[-2] = 0
+                
+            for i in stones:
+                if i == 0: stones.remove(0)
+            
+            x = stones[-1]
+            y = stones[-2]
+            
+            stones.sort()
+            
+        x = stones[-1]
+        y = stones[-2]  
+        print(stones)
+        
+        if x > y:
+                stones[-2] = x - y
+                stones[-1] = 0
+            
+        elif x == y:
+                stones[-1] = 0
+                stones[-2] = 0
+                
+        print(stones)
+        
+        return sum(stones)
+        
