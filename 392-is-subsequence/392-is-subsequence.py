48fe114ad2d3c1a5a24305844e13986d8252ad4e
Atri Patel <64589033+Atri10@users.noreply.github.com>
2022-04-08 14:50:08 +0530

    Time: 16 ms (93.19%), Space: 13.5 MB (66.27%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.py
+++ 392-is-subsequence/392-is-subsequence.py
@@ -5,15 +5,11 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        if len(s)==len(t):
-            if s==t:return True
-            else:return False
-        if(len(s)>len(t)):return False
-        i,j = 0,0 
-        while True:
-            if(j>=len(s)):
-                if(j == len(s)):return True                
-            if(i>=len(t)):break              
-            X,Y = t[i],s[j]          
-            if X==Y:j = j + 1
-            i = i + 1
+        if not s : return True
+        pos = 0
+        for i in t:
+            if i == s[pos]:
+                pos += 1
+                if pos == len(s):
+                    return True
+        return False
