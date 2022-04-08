cc81b84c2a409a63a409bb9ceae3ec81eaaffda8
Atri Patel <64589033+Atri10@users.noreply.github.com>
2022-04-08 19:27:41 +0530

    Time: 48 ms (58.84%), Space: 14.4 MB (14.02%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.py
@@ -0,0 +1,15 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        
+        ans = []
+        def BackTrack(nopen,nclose,path):
+            if n == nclose:
+                ans.append(path)
+                return
+            if n > nopen:BackTrack(nopen+1,nclose,path+ "(")
+            if nopen > nclose: BackTrack(nopen,nclose + 1,path+ ")")
+                
+        
+        BackTrack(0,0,"")
+        return ans
+        
