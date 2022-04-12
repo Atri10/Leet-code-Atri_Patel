4c2183c878ee7695a8ee6a78c0677847262a0445
Atri Patel <64589033+Atri10@users.noreply.github.com>
2022-04-12 09:56:27 +0530

    Time: 41 ms (59.38%), Space: 14 MB (22.31%) - LeetHub

---
+++ 50-powx-n/50-powx-n.py
@@ -0,0 +1,15 @@
+from functools import cache
+
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        
+        
+        @cache
+        def power(x,n):
+            if n==0: return 1
+            elif n==1:return x
+            elif n < 0: return power(1/x,-n)
+            elif n%2==0:return power(x*x,n//2)
+            else: return x * power(x*x,(n-1)//2)
+            
+        return power(x,n)
