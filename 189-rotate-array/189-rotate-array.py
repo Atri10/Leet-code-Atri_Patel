8fbff6eb7931e403665d4b94fd258af4e49b6110
Atri Patel <64589033+Atri10@users.noreply.github.com>
2022-07-21 21:00:17 +0530

    Time: 385 ms (42.05%), Space: 25.3 MB (96.76%) - LeetHub

--- 189-rotate-array/189-rotate-array.py
+++ 189-rotate-array/189-rotate-array.py
@@ -3,20 +3,14 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        k = k % len(nums)
         
-    
         def rev(nums,l,r):
-            while l < r:
+            while l <= r:
                 nums[l],nums[r] = nums[r],nums[l]
-                l = l + 1
-                r = r - 1
+                l += 1
+                r -= 1
         
-        
-        
-        ll = len(nums)
-        k = k % ll;
-        rev(nums,0,ll-k-1)
-        rev(nums,ll-k,ll-1)
-        rev(nums,0,ll-1)
-        
-        
+        rev(nums,0,len(nums)-k-1)
+        rev(nums,len(nums)-k,len(nums)-1)     
+        rev(nums,0,len(nums)-1)        
