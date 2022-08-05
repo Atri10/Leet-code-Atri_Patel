a474e84ac36f0ce89af75165ca7e9bcff668e30f
Atri Patel <64589033+Atri10@users.noreply.github.com>
2022-08-05 23:43:33 +0530

    Time: 54 ms (70.51%), Space: 14.1 MB (18.29%) - LeetHub

---
+++ 377-combination-sum-iv/377-combination-sum-iv.py
@@ -0,0 +1,26 @@
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        
+        memo = {}
+        
+        def back(nums,i,target):
+            
+            if i >= len(nums):return 0
+            
+            if target == 0:return 1
+            
+            if target in memo:
+                return memo[target]
+            
+            res = 0
+            
+            for k in range(len(nums)):
+                if target - nums[k] >= 0:
+                    target -= nums[k]
+                    res += back(nums,k,target)
+                    target += nums[k]
+            
+            memo[target] = res
+            return memo[target]
+        
+        return back(nums,0,target)
