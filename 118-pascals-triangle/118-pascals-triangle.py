bb27b7ac5271c51383fd2833f17dada9dab80a3a
Atri Patel <64589033+Atri10@users.noreply.github.com>
2022-08-16 01:44:25 +0530

    Time: 59 ms (19.60%), Space: 13.8 MB (65.76%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.py
+++ 118-pascals-triangle/118-pascals-triangle.py
@@ -3,24 +3,14 @@ class Solution:
         
         
         triangle = []
-        
-        
         triangle.append([1])
         triangle.append([1,1])
         
-        if numRows < 3:
-            return triangle[:numRows]
+        if numRows < 3:return triangle[:numRows]
         
         for i in range(3,numRows+1):
-            
             prevrow = triangle[-1]
-            
-            cur = []
-            cur.append(1)
-            for k in range(len(prevrow)-1):
-                cur.append(prevrow[k] + prevrow[k+1])
-            
-            cur.append(1)
+            cur = [1] + [prevrow[k] + prevrow[k+1] for k in range(len(prevrow)-1)] + [1]
             triangle.append(cur)
    
 
