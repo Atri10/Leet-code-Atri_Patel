01f9fd7a16d12a71d37a4363999bec692d9a00ee
Atri Patel <64589033+Atri10@users.noreply.github.com>
2022-08-16 14:36:27 +0530

    Time: 65 ms (71.81%), Space: 14.1 MB (68.63%) - LeetHub

---
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.py
@@ -0,0 +1,13 @@
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        
+        dic = {}
+        ans = {}
+        for i in nums1:
+            dic[i] = 0
+            
+        for i in nums2:
+            if i in dic:
+                ans[i] = 0
+                
+        return list(ans.keys())
