e6f36529299c6e0345ac2623913d846ae0c5a841
Atri Patel <64589033+Atri10@users.noreply.github.com>
2022-11-01 13:33:03 +0530

    Time: 541 ms (8.64%), Space: 19.2 MB (25.66%) - LeetHub

--- 0554-brick-wall/0554-brick-wall.py
+++ 0554-brick-wall/0554-brick-wall.py
@@ -1,26 +1,16 @@
-class Solution {
-public:
-    int leastBricks(vector<vector<int>>& wall) 
-    {
-        unordered_map<int, int> edge_frequency;     //HashMap to store the number of common edges among the rows
-
-        int max_frequency = 0;         //Variable to store the frequency of most occuring edge
-        
-        for(int row=0; row<wall.size(); row++)        //Iterating through each row
-        {
-            int edge_postion = 0;       //Variable to store different edge postion
+class Solution:
+    def leastBricks(self, wall: List[List[int]]) -> int:
+        dic = {}
+        MAX = 0
+        for row in wall:
+            endpoint = 0
+            for i in range(len(row) - 1):
+                endpoint += row[i]
+                
+                if endpoint in dic:
+                    dic[endpoint] += 1
+                else:
+                    dic[endpoint] = 1
             
-            for(int brick_no=0; brick_no< wall[row].size() -1; brick_no++)    //Iterating through each brick inside a row
-            { 
-                int current_brick_length = wall[row][brick_no];  //Length of the current brick
-                
-                edge_postion = edge_postion + current_brick_length ;  //Next Edge Position = Previous Edge Position + Current Brick's Length
-                
-                edge_frequency[edge_postion]++;  //Incrementing the Frequency of just calculated Edge Postion
-                
-                max_frequency = max(edge_frequency[edge_postion],max_frequency);  //Comparing the "Frequency of just calculated Edge Postion" with "Max Frequency seen till now" & storing whichever is greater.
-            }
-        }
-        return wall.size() - max_frequency; // returning (Number of Bricks Crossed by Line) i.e. (Number of Rows in Wall - Frequency of Most Occuring Edge) 
-    }
-};
+        c = dic.values()
+        return len(wall) - (max(c) if c else 0)
