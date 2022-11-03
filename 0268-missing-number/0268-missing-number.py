53783a45285ad23fb9a0e4d90d67c148a29a3762
Atri Patel <64589033+Atri10@users.noreply.github.com>
2022-11-03 15:25:47 +0530

    Time: 316 ms (48.74%), Space: 15.7 MB (12.32%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -2,11 +2,11 @@ class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         
         dic = {}
-        
+        n = len(nums)
         for i in nums:
             dic[i] =True
         
-        n = len(nums)
+        del(nums)
         mis = 0
         for i in range(n+1):
             if i not in dic: mis = i
